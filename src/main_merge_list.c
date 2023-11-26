107deb9514af49e5a0ce680660b975adda7b6d88
Enrico Barbin <e.barbin@studenti.unipi.it>
2023-11-26 20:36:47 +0100

    merge list function

--- src/main_merge_list.c
+++ src/main_merge_list.c
@@ -11,13 +11,25 @@ typedef struct nodo node_t;
 
 extern node_t *merge(node_t *prima, node_t *seconda);
 
+void print_list(node_t* list){
+    node_t* t = list ;
+    printf("[ ");
+    while (t != NULL) {
+        printf("%d ", t->value);
+        t = t->next;
+    }
+    printf("]\n");
+}
+
+
+//Sto main l'ha scritto il mencagli ho solo aggiunto cose per stampare
+//Pieno di memory leak brutto codice assai però me lo tengo a fini didattici
 int main()
 {
     // creo la prima lista
-    int i=0;
     node_t *prima_list = NULL;
     node_t *prec = NULL;
-    for (i=0; i<5; i++) {
+    for (int i=0; i<5; i++) {
         node_t *elem = (node_t *) malloc(sizeof(node_t));
         if (elem == NULL) {
             printf("Errore malloc\n");
@@ -34,10 +46,11 @@ int main()
         }
         elem->next = NULL;
     }
+    print_list(prima_list);
     // creo la seconda lista
     node_t *seconda_list = NULL;
     prec = NULL;
-    for (; i<10; i++) {
+    for (int i = 0; i<10; i++) {
         node_t *elem = (node_t *) malloc(sizeof(node_t));
         if (elem == NULL) {
             printf("Errore malloc\n");
@@ -54,13 +67,9 @@ int main()
         }
         elem->next = NULL;
     }
+    print_list(seconda_list);
     node_t *result_list = merge(prima_list, seconda_list);
+    print_list(result_list);
     // stampo la lista unita
-    printf("[ ");
-    while (result_list != NULL) {
-        printf("%d ", result_list->value);
-        result_list = result_list->next;
-    }
-    printf("]\n");
     return 0;
 }
--- src/merge_list.s
+++ src/merge_list.s
@@ -5,12 +5,16 @@
 merge :
         ands r2 , r0 , r1 @ list a || list b == NULL? => r2 
         beq null @ eq -> Z == 1 ?
-        @bla bla
-
-
-
+        mov r2 , r0  @ r2 = prima_lista 
+    next :
+        cmp r2 , #0 
+        beq end
+        mov r3 , r2 @ r3 = p 
+        ldr r2 , [r3, #4] @ r2 = p->next 
+        b next
+    end :
+        str r1 , [r3 , #4]
         mov pc , lr
-
 
     null :
         ldr r0 , =errmsg
